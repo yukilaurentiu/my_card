ed146941584b458d2be34f6c8a230636c6fe5017
yuki <yuki.laurentiu@gmail.com>
2023-04-20 14:02:18 -0600

    added fontawesome icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter/src/material/colors.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:my_card/challenge.dart';
 
 void main() {
@@ -39,6 +39,44 @@ class MyApp extends StatelessWidget {
                     letterSpacing: 1.5,
                     fontWeight: FontWeight.bold,
                   )),
+              Container(
+                color: Colors.white,
+                padding: const EdgeInsets.all(10.0),
+                margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    const FaIcon(FontAwesomeIcons.phone, color: Colors.teal),
+                    const SizedBox(
+                      width: 10.0,
+                    ),
+                    Text('+ 1 604 356 5379',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 15.0,
+                        )),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: const EdgeInsets.all(10.0),
+                margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    const Icon(FontAwesomeIcons.envelope, color: Colors.teal),
+                    const SizedBox(
+                      width: 10.0,
+                    ),
+                    Text('yuki.laurentiu@gmail.com',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 15.0,
+                        )),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
